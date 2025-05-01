e53581469e9bd7831ca5f669183c1521db5e3dcf
Aditya-IITM-BS <adityabaxla@study.iitm.ac.in>
2025-05-01 14:22:47 +0530

    added associations in index.js

--- database/models/index.js
+++ database/models/index.js
@@ -11,13 +11,57 @@ const FeeType = require("./FeeType");
 const FeeStructure = require("./FeeStructure");
 const FeeInvoice = require("./FeeInvoice");
 const FeePayment = require("./FeePayment");
+const Job = require("./Job");
 
-// make the associations
-Student.belongsTo(ClassSection);
-ClassSection.hasMany(Student);
+// === Associations ===
 
-ClassSection.belongsTo(Class);
-Class.hasMany(ClassSection);
+// Student ↔ Enrollment (1-to-Many)
+Student.hasMany(Enrollment, { foreignKey: "studentId" });
+Enrollment.belongsTo(Student, { foreignKey: "studentId" });
+
+// ClassSection ↔ Enrollment (1-to-Many)
+ClassSection.hasMany(Enrollment, { foreignKey: "classSectionId" });
+Enrollment.belongsTo(ClassSection, { foreignKey: "classSectionId" });
+
+// AcademicYear ↔ Enrollment (1-to-Many)
+AcademicYear.hasMany(Enrollment, { foreignKey: "academicYearId" });
+Enrollment.belongsTo(AcademicYear, { foreignKey: "academicYearId" });
+
+// Class ↔ ClassSection (1-to-Many)
+Class.hasMany(ClassSection, { foreignKey: "classId" });
+ClassSection.belongsTo(Class, { foreignKey: "classId" });
+
+// AcademicYear ↔ Configuration (1-to-Many)
+AcademicYear.hasMany(Configuration, { foreignKey: "academicYearId" });
+Configuration.belongsTo(AcademicYear, { foreignKey: "academicYearId" });
+
+// === Fee Structure & Payments ===
+
+// AcademicYear ↔ FeeStructure (1-to-Many)
+AcademicYear.hasMany(FeeStructure, { foreignKey: "academicYearId" });
+FeeStructure.belongsTo(AcademicYear, { foreignKey: "academicYearId" });
+
+// Class ↔ FeeStructure (1-to-Many)
+Class.hasMany(FeeStructure, { foreignKey: "classId" });
+FeeStructure.belongsTo(Class, { foreignKey: "classId" });
+
+// FeeType ↔ FeeStructure (1-to-Many)
+FeeType.hasMany(FeeStructure, { foreignKey: "feeTypeId" });
+FeeStructure.belongsTo(FeeType, { foreignKey: "feeTypeId" });
+
+// Student ↔ FeeInvoice (1-to-Many)
+Student.hasMany(FeeInvoice, { foreignKey: "studentId" });
+FeeInvoice.belongsTo(Student, { foreignKey: "studentId" });
+
+// FeeStructure ↔ FeeInvoice (1-to-Many)
+FeeStructure.hasMany(FeeInvoice, { foreignKey: "feeStructureId" });
+FeeInvoice.belongsTo(FeeStructure, { foreignKey: "feeStructureId" });
+
+// FeeInvoice ↔ FeePayment (1-to-Many)
+FeeInvoice.hasMany(FeePayment, { foreignKey: "feeInvoiceId" });
+FeePayment.belongsTo(FeeInvoice, { foreignKey: "feeInvoiceId" });
+
+// You can add Job associations here if it relates to any other models
 
 module.exports = {
   sequelize,
@@ -31,4 +75,5 @@ module.exports = {
   FeeStructure,
   FeeInvoice,
   FeePayment,
+  Job,
 };
